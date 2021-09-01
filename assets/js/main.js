f8dec5ee2dc4652a404b81ba21c28df70cfe0335
Engoa <idobz1234567890@gmail.com>
2021-09-01 18:42:13 +0300

    added npm and a pollem module

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,40 @@
-// Hide/Show Navigation bar on mobile/desktop
-const menuList = document.getElementById('menuList');
-const closeNavElement = document.getElementById('closenav');
+  // Hide/Show Navigation bar on mobile/desktop
+  const menuList = document.getElementById('menuList');
+  const closeNavElement = document.getElementById('closenav');
 
-function menuToggle() {
-  menuList.classList.toggle('mobilebtn');
-}
-// Hide/Show Navigation bar on mobile/desktop
-
-// Close Navbar on mobile when clicking links
-function closeNav() {
-  menuList.classList.remove('mobilebtn');
-}
-// Close Navbar on mobile when clicking links
-
-const goToTopBtn = document.getElementById('gototop');
-const gotoTopWrapper = document.querySelector('.gototop');
-// When the user scrolls down 20px from the top of the document, show the button
-const isScrolled = () => {
-  const docScrollTop = document.documentElement.scrollTop;
-  console.log(docScrollTop);
-  if (docScrollTop >= 400) {
-    goToTopBtn.style.display = 'flex';
-    gotoTopWrapper.style.opacity = 1;
-    gotoTopWrapper.style.visibility = 'visible';
-  } else {
-    gotoTopWrapper.style.opacity = 0;
-    gotoTopWrapper.style.visibility = 'hidden';
+  function menuToggle() {
+    menuList.classList.toggle('mobilebtn');
   }
-};
+  // Hide/Show Navigation bar on mobile/desktop
 
-window.onscroll = () => {
-  isScrolled();
-};
+  // Close Navbar on mobile when clicking links
+  function closeNav() {
+    menuList.classList.remove('mobilebtn');
+  }
+  // Close Navbar on mobile when clicking links
 
-// When the user clicks on the button, scroll to the top of the document
-goToTopBtn.addEventListener('click', () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+  const goToTopBtn = document.getElementById('gototop');
+  const gotoTopWrapper = document.querySelector('.gototop');
+  // When the user scrolls down 20px from the top of the document, show the button
+  const isScrolled = () => {
+    const docScrollTop = document.documentElement.scrollTop;
+    console.log(docScrollTop);
+    if (docScrollTop >= 400) {
+      goToTopBtn.style.display = 'flex';
+      gotoTopWrapper.style.opacity = 1;
+      gotoTopWrapper.style.visibility = 'visible';
+    } else {
+      gotoTopWrapper.style.opacity = 0;
+      gotoTopWrapper.style.visibility = 'hidden';
+    }
+  };
+
+  window.onscroll = () => {
+    isScrolled();
+  };
+
+  // When the user clicks on the button, scroll to the top of the document
+  goToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
