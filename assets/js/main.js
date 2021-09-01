ffe692cf99dc4a28ae9067dc458ca4d20d88c6f7
Engoa <idobz1234567890@gmail.com>
2021-09-02 02:47:07 +0300

    overhaul of colors on website, cleaned up code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,39 @@
-  // Hide/Show Navigation bar on mobile/desktop
-  const menuList = document.getElementById('menuList');
-  const closeNavElement = document.getElementById('closenav');
+// Hide/Show Navigation bar on mobile/desktop
+const menuList = document.getElementById('menuList');
+const closeNavElement = document.getElementById('closenav');
 
-  function menuToggle() {
-    menuList.classList.toggle('mobilebtn');
+function menuToggle() {
+  menuList.classList.toggle('mobilebtn');
+}
+// Hide/Show Navigation bar on mobile/desktop
+
+// Close Navbar on mobile when clicking links
+function closeNav() {
+  menuList.classList.remove('mobilebtn');
+}
+// Close Navbar on mobile when clicking links
+
+const goToTopBtn = document.getElementById('gototop');
+const gotoTopWrapper = document.querySelector('.gototop');
+// When the user scrolls down 20px from the top of the document, show the button
+const isScrolled = () => {
+  const docScrollTop = document.documentElement.scrollTop;
+  if (docScrollTop >= 400) {
+    goToTopBtn.style.display = 'flex';
+    gotoTopWrapper.style.opacity = 0.9;
+    gotoTopWrapper.style.visibility = 'visible';
+  } else {
+    gotoTopWrapper.style.opacity = 0;
+    gotoTopWrapper.style.visibility = 'hidden';
   }
-  // Hide/Show Navigation bar on mobile/desktop
+};
 
-  // Close Navbar on mobile when clicking links
-  function closeNav() {
-    menuList.classList.remove('mobilebtn');
-  }
-  // Close Navbar on mobile when clicking links
+window.onscroll = () => {
+  isScrolled();
+};
 
-  const goToTopBtn = document.getElementById('gototop');
-  const gotoTopWrapper = document.querySelector('.gototop');
-  // When the user scrolls down 20px from the top of the document, show the button
-  const isScrolled = () => {
-    const docScrollTop = document.documentElement.scrollTop;
-    if (docScrollTop >= 400) {
-      goToTopBtn.style.display = 'flex';
-      gotoTopWrapper.style.opacity = 1;
-      gotoTopWrapper.style.visibility = 'visible';
-    } else {
-      gotoTopWrapper.style.opacity = 0;
-      gotoTopWrapper.style.visibility = 'hidden';
-    }
-  };
-
-  window.onscroll = () => {
-    isScrolled();
-  };
-
-  // When the user clicks on the button, scroll to the top of the document
-  goToTopBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
+// When the user clicks on the button, scroll to the top of the document
+goToTopBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
