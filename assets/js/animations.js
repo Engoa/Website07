c9fa3bfe4acdd98add7fa2064b19360e018adc61
Engoa <idobz1234567890@gmail.com>
2021-09-08 20:20:43 +0300

    fixes for main home page stylings

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,11 +1,11 @@
-function animatePage(selector) {
-  gsap
-    .from(document.querySelectorAll(selector), {
-      autoAlpha: 0,
-      y: 100,
-      ease: Power1.ease,
-      stagger: 0.2,
-      clearProps: 'all',
-    })
-    .totalDuration(1.5);
-}
+// function animatePage(selector) {
+//   gsap
+//     .from(document.querySelectorAll(selector), {
+//       autoAlpha: 0,
+//       y: 100,
+//       ease: Power1.ease,
+//       stagger: 0.2,
+//       clearProps: 'all',
+//     })
+//     .totalDuration(1.5);
+// }
